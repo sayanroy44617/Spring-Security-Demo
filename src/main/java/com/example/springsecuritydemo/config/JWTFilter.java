b08c4cd3b8cb38772bf0751f489e66144171bc4d
Sayan Roy <sayanroy44617@gmail.com>
2026-08-02 00:31:16 +0530

    Annotate `JWTFilter` class with @Configuration for better Spring context integration.

--- src/main/java/com/example/springsecuritydemo/config/JWTFilter.java
+++ src/main/java/com/example/springsecuritydemo/config/JWTFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @AllArgsConstructor
+@Configuration
 public class JWTFilter extends OncePerRequestFilter {
 
 
